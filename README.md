ea277fe94017da7606aae620cfbc84c59a6dad37
greatpie <pie@dyclub.org>
2019-03-17 17:28:42 +0800

    1. Revert layout size according to device height 2. Modify README

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 A simple cross-platform game built with Flutter.
 
 ## Game Tips
-1. Just tap the moving ant to prevent it from stealing doughnuts.Once a ant touch doughnuts,game is over.
-2. More ants you have tapped,higher score you can acquire.
+1. Just tap the moving ant to prevent it from stealing doughnuts.Once a ant touches doughnuts,the game is over.
+2. The more ants you have tapped,the higher score you can acquire.
 3. Be careful!The ants move faster and faster.
 
 ## About
 ### contributors
 - developer: [Jiapai Liu](https://greatpie.me/)
-- designer: TianZhen Li
+- designer: Tianzhen Li
 
 ### dependence
 - [flutter_screenutil](https://github.com/OpenFlutter/flutter_screenutil), A flutter plugin for adapting screen and font size.Let your UI display a reasonable layout on different screen sizes!
--- lib/main.dart
+++ lib/main.dart
@@ -5,60 +5,78 @@ void main() {
   runApp(MaterialApp(
     title: 'Help Doughnuts',
     home: DoughnutsApp(),
-    theme: _customTheme,
   ));
 }
-var _customTheme =ThemeData(
-  primaryColor: Colors.black,
-  backgroundColor: Colors.white,
-);
+
 class DoughnutsApp extends StatefulWidget {
   @override
   _DoughnutsAppState createState() => _DoughnutsAppState();
 }
 
 class _DoughnutsAppState extends State<DoughnutsApp> {
+  int _score = 0;
+
+  void _increaseScore() {
+    setState(() {
+      _score++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
-//    double statusH = ScreenUtil.statusBarHeight;
 
-    double rightPosition = 20.0;
-    var textStyle = TextStyle(
+    final _rightPosition = 20.0;
+    final _textStyle = TextStyle(
         color: Colors.black,
-        fontSize: ScreenUtil(allowFontScaling: true).setSp(24),
         fontWeight: FontWeight.normal,
         decoration: TextDecoration.none);
 
+//    set size
+    var _h = (size) => ScreenUtil().setHeight(size);
+
     return Container(
       color: Colors.white,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[     
-          Positioned(
-            top: ScreenUtil().setSp(80),
-            right: rightPosition,
-            child: Text(
-              'Score',
-              style: textStyle,
+      child: DefaultTextStyle(
+        style: _textStyle,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: _h(80),
+              right: _rightPosition,
+              child: Text(
+                'Score',
+                style: TextStyle(fontSize: _h(24)),
+              ),
             ),
-          ),
-          Positioned(
-            top: ScreenUtil().setSp(130),
-            right: rightPosition,
-            child: Text(
-              '100',
-              style: textStyle,
+            Positioned(
+              top: _h(130),
+              right: _rightPosition,
+              child: Text(
+                '$_score',
+                style: TextStyle(fontSize: _h(49)),
+              ),
             ),
-          ),
-          Positioned(
-            top: ScreenUtil().setSp(373),
-            child: Image(
-              image: AssetImage("assets/donuts.png"),
-              height: ScreenUtil().setHeight(156),
+            Positioned(
+              top: _h(373),
+              child: Image(
+                image: AssetImage("assets/donuts.png"),
+                height: _h(156),
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              top: _h(840),
+              child: GestureDetector(
+                child: Image(
+                  image: AssetImage("assets/eggs_ant.png"),
+                  height: _h(76),
+                ),
+                onTap: _increaseScore,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
