42e391c8c340a373840d9013ffe9c0148e97b816
greatpie <pie@dyclub.org>
2019-04-27 03:48:37 +0800

    Update animation resource

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie_flutter/lottie_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(MaterialApp(
@@ -14,17 +19,59 @@ class DoughnutsApp extends StatefulWidget {
   _DoughnutsAppState createState() => _DoughnutsAppState();
 }
 
-class _DoughnutsAppState extends State<DoughnutsApp> {
+class _DoughnutsAppState extends State<DoughnutsApp>
+    with SingleTickerProviderStateMixin {
   int _score = 0;
-
   final _rightPosition = 20.0;
   final _textStyle = TextStyle(
       color: Colors.black,
       fontWeight: FontWeight.normal,
       decoration: TextDecoration.none);
 
+  LottieComposition _composition;
+  String _assetName;
+  AnimationController _controller;
+  bool _repeat;
+
+  @override
+  initState() {
+    super.initState();
+
+    _repeat = false;
+    _assetName = 'assets/animation/ant.json';
+    _controller = new AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    );
+    _controller.addListener(() => setState(() {}));
+    _controller.repeat();
+
+    loadAsset(_assetName).then((LottieComposition composition) {
+      setState(() {
+        _composition = composition;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _loadButtonPressed(String assetName) {
+    loadAsset(assetName).then((LottieComposition composition) {
+      setState(() {
+        _assetName = assetName;
+        _composition = composition;
+        _controller.reset();
+      });
+    });
+  }
+
   void _increaseScore() {
     setState(() {
+      // _controller.repeat();
       _score++;
     });
   }
@@ -68,9 +115,10 @@ class _DoughnutsAppState extends State<DoughnutsApp> {
             Positioned(
               top: _h(840),
               child: GestureDetector(
-                child: SvgPicture.asset(
-                  'assets/svg/ant.svg',
-                  height: _h(76),
+                child: Lottie(
+                  composition: _composition,
+                  size: Size(200.0, 200.0),
+                  controller: _controller,
                 ),
                 onTap: _increaseScore,
               ),
@@ -80,4 +128,11 @@ class _DoughnutsAppState extends State<DoughnutsApp> {
       ),
     );
   }
+}
+
+Future<LottieComposition> loadAsset(String assetName) async {
+  return await rootBundle
+      .loadString(assetName)
+      .then<Map<String, dynamic>>((String data) => json.decode(data))
+      .then((Map<String, dynamic> map) => new LottieComposition.fromMap(map));
 }
